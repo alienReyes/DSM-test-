978a6159908ff4e8d05b121a0c9e2236e52fba20
Rodrigo Reyes Sanchez <kid.alien@gmail.com>
2018-09-24 11:02:21 -0400

    first ass automated download

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,34 @@ const cleanCSS = require('gulp-clean-css');
 const htmlmin = require('gulp-htmlmin');
 const cache = require('gulp-cache');
 const concat = require('gulp-concat');
+const downloadStream=require('gulp-download-stream');
+const minify = require('gulp-babel-minify');
+var rename = require("gulp-rename");
+var runSequence = require('run-sequence');
+var wait = require('gulp-wait');
+
+
+
+// download INVSION DSM FILE
+// HTML MINIFICATION
+gulp.task('download', function () {
+    downloadStream("https://projects.invisionapp.com/dsm-export/vectorworks-web-team/vectorworks/_style-params.scss?key=H1dDT-xF7")
+    .pipe(rename("dsm.scss"))
+    .pipe(gulp.dest("./src/scss/")) 
+    .on('end',function(){
+      gulp.start('sass');
+       console.log("download complete")
+    }) 
+    
+    
+});
+
+
 
 // BROWSER-SYNC / LIVE RELOADING
 gulp.task('browser-sync', () => {
     browserSync.init({
+        browser: "google chrome",
         server: {
             baseDir: './dist'
         },
@@ -18,17 +42,24 @@ gulp.task('browser-sync', () => {
 });
 
 // WATCHING FOR FILE CHANGES, THEN RELOADS
-gulp.task('watch', ['browser-sync'], () => {
-    gulp.watch('src/scss/**/*.scss', ['sass'])
-    gulp.watch('src/*.html', reload)
+gulp.task('watch', ['browser-sync'], () => {   
+    gulp.watch('src/scss/**/*.scss',['download'])
+    gulp.watch('src/*.html', ['html'])
     gulp.watch('src/scripts/**/*.js', ['scripts'])
+   
 });
+
+gulp.task('updateDSM', function () {
+  // runSequence('download','sass'); 
+});
+
 
 // HTML MINIFICATION
 gulp.task('html', function () {
-    return gulp.src('*.html')
+    return gulp.src('src/*.html')
         .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest('public/'))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(reload({ stream: true }))
 });
 
 
@@ -36,6 +67,7 @@ gulp.task('html', function () {
 // SASS INTO CSS THEN AUTOPREFIXING THEN MINIFY
 gulp.task('sass', () => {
     return gulp.src('src/scss/styles.scss')
+        
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer('last 2 versions'))
         .pipe(cleanCSS())
@@ -49,16 +81,11 @@ gulp.task('sass', () => {
 // ES6 TO ES5 VIA BABEL, THEN MINIFES
 gulp.task('scripts', () => {
     gulp.src(['src/scripts/smooth-scroll.js', 'dev/scripts/script.js'])
-        .pipe(concat('main.js'))        
-        .pipe(minify({
-            mangle: {
-                keepClassName: true
-            }
-        }))
+        .pipe(concat('main.js'))       
         .pipe(gulp.dest('dist/js/'))
         .pipe(reload({ stream: true }))
 });
 
 // ALL TASKS INTO ONE INIT
 // ACTIVATE BUT RUNNING 'GULP' IN THE TERMINAL
-gulp.task('default', ['browser-sync', 'html', 'sass', 'scripts', 'watch']);
+gulp.task('default', ['download','html', 'scripts', 'browser-sync','watch']);
